bca00364fab71a9242def9b8910679d3af7a0eb2
Eyguilo <jaumeeltoro@gmail.com>
2023-05-10 02:15:41 +0200

    10/05/23 - Arreglado la maquetación de createBooking.

--- bicycleRenting/src/view/createBookingView.php
+++ bicycleRenting/src/view/createBookingView.php
@@ -1,15 +1,23 @@
 <?php
-ini_set('display_errors', 'On');
-ini_set('html_errors', 1);
-
 session_start();
 $userId = $_SESSION['userId'];
 if (!isset($userId)) {
     header("Location: logInView.php");
 }
 
-require_once("../logic/createBookingBusinessLaw.php");
-$createBookingBusinessLaw = new CreateBookingBusinessLaw();
+require_once("../logic/listBicyclesBusinessLaw.php");
+$listBicyclesBusinessLaw = new ListBicyclesBusinessLaw();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $listBicyclesBusinessLaw = new ListBicyclesBusinessLaw();
+    $filterData = array($_POST['brand'], $_POST['model'], $_POST['size']);
+    $filteredBicycles = $listBicyclesBusinessLaw->findBicycles($filterData);
+} else {
+    $falseFilter = "";
+    $filteredBicycles = $listBicyclesBusinessLaw->findBicycles($falseFilter);
+}
+
+
 
 ?>
 <!DOCTYPE html>
@@ -19,17 +27,17 @@ $createBookingBusinessLaw = new CreateBookingBusinessLaw();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create booking</title>
-    <link rel="stylesheet" href="../../css/createBooking.css">
+    <title>List bicycles</title>
+    <link rel="stylesheet" href="../../css/createBooking copy.css">
 </head>
 
 <body>
     <div id="container">
-        <div id="central">
+        <div id="back-button">
+            <a href="menuStartView.php">Home</a>
+        </div>
+        <div id="centralBooking">
             <div id="create">
-                <div id="back-button">
-                    <a href="menuStartView.php">Home</a>
-                </div>
                 <div class="title">Create Booking</div>
                 <form method="POST" action="createBookingView.php">
                     <div class="form-group">
@@ -39,7 +47,8 @@ $createBookingBusinessLaw = new CreateBookingBusinessLaw();
                     </div>
                     <div class="form-group">
                         <label for="clientName">Client:</label>
-                        <input type="text" id="clientName" name="clientName" autocomplete="off" placeholder="ex. Jaume Aguiló"><br><br>
+                        <input type="text" id="clientName" name="clientName" autocomplete="off"
+                            placeholder="ex. Jaume Aguiló"><br><br>
                         <select id="clientList" name="clientList">
                             <option value="" selected>Select a client</option>
                         </select>
@@ -76,9 +85,105 @@ $createBookingBusinessLaw = new CreateBookingBusinessLaw();
                 </form>
             </div>
         </div>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="../../js/findClients.js"></script>
+        <div id="centralList">
+            <div id="create">
+                <div class="table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                                    <th>
+                                        <label for="brand">Brand:</label>
+                                        <select id="brand" name="brand" onchange="this.form.submit()">
+                                            <?php
+                                            $resultBrands = $listBicyclesBusinessLaw->findBrands();
+                                            $valueBrand = 1;
+                                            $selectedBrand = isset($_POST['brand']) ? $_POST['brand'] : "";
+                                            echo "<option value='' selected>Select a brand</option>";
+                                            foreach ($resultBrands as $brand) {
+                                                $selected = ($selectedBrand == $valueBrand) ? "selected" : "";
+                                                echo "<option value='" . $valueBrand . "' " . $selected . ">" . $brand[0] . "</option>";
+                                                $valueBrand++;
+                                            }
+                                            ?>
+                                        </select>
+                                    </th>
+                                    <th>
+                                        <label for="model">Model:</label>
+                                        <select id="model" name="model" onchange="this.form.submit()">
+                                            <?php
+                                            $resultModels = $listBicyclesBusinessLaw->findModels();
+                                            $valueModel = 1;
+                                            $selectedModel = isset($_POST['model']) ? $_POST['model'] : "";
+                                            echo "<option value='' selected>Select a model</option>";
+                                            foreach ($resultModels as $model) {
+                                                $selected = ($selectedModel == $valueModel) ? "selected" : "";
+                                                echo "<option value='" . $valueModel . "' " . $selected . ">" . $model[0] . "</option>";
+                                                $valueModel++;
+                                            }
+                                            ?>
+                                        </select>
+                                    </th>
+                                    <th>
+                                        <label for="size">Size:</label>
+                                        <select id="size" name="size" onchange="this.form.submit()">
+                                            <?php
+                                            $resultsizes = $listBicyclesBusinessLaw->findSizes();
+                                            $valueSize = 1;
+                                            $selectedSize = isset($_POST['size']) ? $_POST['size'] : "";
+                                            echo "<option value='' selected>Select a size</option>";
+                                            foreach ($resultsizes as $size) {
+                                                $selected = ($selectedSize == $valueSize) ? "selected" : "";
+                                                echo "<option value='" . $valueSize . "' " . $selected . ">" . $size[0] . "</option>";
+                                                $valueSize++;
+                                            }
+                                            ?>
+                                        </select>
+                                    </th>
+                                    <th>
+                                        <label for="brand">Color</label>
+                                    </th>
+                                    <th>
+                                        <label for="brand">Price / hour</label>
+                                    </th>
+                                    <th>
+                                        <label for="brand">Available</label>
+                                    </th>
+                                    <th>
+                                        <label for="brand">Reserve</label>
+                                    </th>
+                                </form>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+
+                            foreach ($filteredBicycles as $bike) {
+
+                                echo "
+                                        <tr>
+                                            <td>" . $bike['id'] . "</td>
+                                            <td>" . $bike['brand'] . "</td>
+                                            <td>" . $bike['model'] . "</td>
+                                            <td>" . $bike['size'] . "</td>
+                                            <td>" . $bike['color'] . "</td>
+                                            <td>" . $bike['price'] . "</td>
+                                            <td>" . $bike['available'] . "</td>
+                                            <td>.WORKING ON IT.</td>
+                                        </tr>";
+                            }
+
+                            ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="../../js/findClients.js"></script>
 </body>
 
 </html>
