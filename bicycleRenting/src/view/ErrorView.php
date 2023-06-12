59cf08f97a4096ccac3a19b2468078fce752e4b8
Eyguilo <jaumeeltoro@gmail.com>
2023-06-12 16:24:06 +0200

    12/06/2023 - Últimos detalles v2.

---
+++ bicycleRenting/src/view/ErrorView.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Error</title>
+    <link rel="stylesheet" href="../../css/error.css">
+</head>
+<body>
+    <div id="container">
+        <div id="central">
+            <h1 class="title">Apologies, we are currently unable to connect to the data.</h1>
+            <div class="links">
+                <a href="LogInView.php">Log in</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
